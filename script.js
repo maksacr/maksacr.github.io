da7d1c7c016aac52e63458a7a8219354bd5cbfc7
arkx <157208180+maksacr@users.noreply.github.com>
2025-04-03 16:16:03 +0530

    Maksacrweb

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Website is running!");
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevents page reload
+        window.open(this.href, '_blank'); // Opens the link in a new tab
+    });
+});
